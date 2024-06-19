3127dd4530bf0e3c7106d3584ede8ecd533d4199
Rishav Harshit <121596454+Rishav82@users.noreply.github.com>
2024-06-19 14:47:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -50,4 +50,9 @@ if st.button('Predict'):
         
     test = np.array([year, km_driven, fuel, seller_type, transmission, owner])
     test = test.reshape(1, 6)
-    st.success(model.predict(test)[0])
+
+
+    prediction = model.predict(test)[0]
+
+    # Display the result
+    st.success(f'Predicted Price: {prediction}')
